6a65e7985f0a5285410b7e4397cf7136b81ec65b
Pedro Henrique <ph6353752@gmail.com>
2025-10-18 21:04:54 -0300

    Maior de Dois

---
+++ src/ExerciciosLogica/maiorDeDois.java
@@ -0,0 +1,27 @@
+package ExerciciosLogica;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class maiorDeDois {
+    public static void main (String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n1, n2;
+
+        System.out.print("Digite dois números para achar o maior entre eles: ");
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+
+        if (n1 > n2){
+            System.out.println("O maior entre os dois é o: " + n1);
+        }
+        else {
+            System.out.println("O maior entre os dois é o: "+  n2);
+        }
+
+        sc.close();
+    }
+}
--- src/ExerciciosLogica/parOuImpar.java
+++ src/ExerciciosLogica/parOuImpar.java
@@ -1,9 +1,9 @@
-package Data;
+package ExerciciosLogica;
 
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
+public class parOuImpar {
     public static void main (String[] args){
 
         Locale.setDefault(Locale.US);
